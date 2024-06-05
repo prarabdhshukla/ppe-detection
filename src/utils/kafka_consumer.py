3cd4d1af66dd5a862a0f0286cadff208a1b95df0
prarabdhshukla <prarabdh.10@gmail.com>
2024-06-05 19:30:11 +0530

    Integrated PostGres with Kafka Consumer

--- src/utils/kafka_consumer.py
+++ src/utils/kafka_consumer.py
@@ -1,6 +1,8 @@
+import psycopg2
 from kafka import KafkaConsumer, KafkaProducer
 import cv2
 import numpy as np
+import json
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -16,6 +18,13 @@ class FrameConsumer:
         self.frame_detector = FrameDetector()
         self.person_detector = PersonDetector()
         self.ppe_detector = PPEDetector()
+        self.conn = psycopg2.connect(
+            dbname="ppe_detection",
+            user="user",
+            password="password",
+            host="db"
+        )
+        self.cur = self.conn.cursor()
 
     def consume_and_process(self):
         for message in self.consumer:
@@ -25,6 +34,17 @@ class FrameConsumer:
             person_results = self.person_detector.detect_person(frame)
             if is_person_detected(person_results):
                 ppe_results = self.ppe_detector.detect_ppe(frame)
-                if not is_ppe_present(ppe_results):
+                detected_ppe = is_ppe_present(ppe_results)
+                self.save_results_to_db("camera1", frame, detected_ppe,ppe_results)
+                if not detected_ppe:
                     _, buffer = cv2.imencode('.jpg', frame)
                     self.producer.send(self.output_topic, buffer.tobytes())
+    
+    def save_results_to_db(self, camera_id,frame,detected_ppe,details):
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+        self.cur.execute(
+            "INSERT INTO results (camera_id, timestamp, frame, detected_ppe, details) VALUES (%s, NOW(), %s, %s, %s)",
+            (camera_id, frame_bytes, detected_ppe, json.dumps(details.tolist()))
+        )
+        self.conn.commit()
